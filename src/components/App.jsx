6e6d8f9f5085224ddc50d5b7080b21b6947bedd8
Andrii Horbovskyi <117573529+g0rbochek@users.noreply.github.com>
2024-02-24 18:07:00 +0100

    secondChek

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { TitleContacts } from './TitleContext/TitleContext';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
+import runtime from './runtime.js';
 
 export const App = () => {
   // const [contacts, setContacts] = useState(
